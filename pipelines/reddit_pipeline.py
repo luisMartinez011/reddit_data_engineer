177bcec5db6cb70435d559aa19d1bb6eaaa8a625
Luis Martinez <luis.martinez33@protonmail.com>
2025-09-21 20:16:05 -0600

    debugging logs

--- pipelines/reddit_pipeline.py
+++ pipelines/reddit_pipeline.py
@@ -16,4 +16,4 @@ def reddit_pipeline(file_name: str, subreddit: str, time_filter='day', limit=Non
     file_path = f'{OUTPUT_PATH}/{file_name}.csv'
     load_data_to_csv(post_df, file_path)
 
-    return file_pathc
+    return file_path
